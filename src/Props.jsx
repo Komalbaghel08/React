30d0f27008e2ce5643cad07ed5f00fcb6c354c41
Komalbaghel08 <baghelkomal512@gmail.com>
2025-09-09 16:28:12 +0530

    update

---
+++ src/Props.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Child from "./Child";   // C capital likhna hai
+
+function Parent() {
+  return (
+    <div>
+      <h1>Parent Component</h1>
+      <Child name="Komal" />
+      <Child name="Rahul" />
+    </div>
+  );
+}
+
+export default Props;

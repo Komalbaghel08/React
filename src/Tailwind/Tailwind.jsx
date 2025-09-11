b92af6fbb35bfcaa8763a1adb2cd18ad95bc5079
Komalbaghel08 <baghelkomal512@gmail.com>
2025-09-11 16:05:47 +0530

    update

--- src/Tailwind/Tailwind.jsx
+++ src/Tailwind/Tailwind.jsx
@@ -11,13 +11,19 @@ function Navbar() {
       <h1 className="text-xl font-bold">MyWebsite</h1>
 
       <ul className="flex gap-6">
-        <li><a href="#" className="hover:text-gray-200">Home</a></li>
-        <li><a href="#" className="hover:text-gray-200 ">About</a></li>
-        <li><a href="#" className="hover:text-gray-200">Services</a></li>
-        <li><a href="#" className="hover:text-gray-200">Contact</a></li>
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Contact</li>
         <button className='border rounded m-0.5 p-0.5 bg-fuchsia-300'>Search</button>
       </ul>
     </nav>
+    <main>
+
+      <section className='w-full h-screen border bg-[url()]'>
+
+      </section>
+    </main>
     <div className='align-middle'><img src={pic} alt="" /></div>
     </>
   );

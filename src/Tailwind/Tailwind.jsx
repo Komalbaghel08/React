fd7b026eafd65f8680d0a31a217d5457306d8981
Komalbaghel08 <baghelkomal512@gmail.com>
2025-09-12 16:27:46 +0530

    update

--- src/Tailwind/Tailwind.jsx
+++ src/Tailwind/Tailwind.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./Tailwind.css"
 import pic from '../assets/download.jpeg';
+import pic1 from '../assets/download (2).jpeg';
+import pic2 from '../assets/images.jpeg';
 
 
 function Navbar() {
@@ -10,7 +12,7 @@ function Navbar() {
     
       <h1 className="text-xl font-bold">MyWebsite</h1>
 
-      <ul className="flex gap-6 hidden lg:flex gap-2">
+      <ul className="flex gap-6">
         <li>Home</li>
         <li>About</li>
         <li>Services</li>
@@ -18,12 +20,31 @@ function Navbar() {
         <button className='border rounded m-0.5 p-0.5 bg-fuchsia-300'>Search</button>
       </ul>
     </nav>
- <div className='h-70 w-70 border '></div>
- <div className='h-70 w-70 border '></div>
- <div className='h-70 w-70 border '></div>
- <div className='h-70 w-70 border '></div>
- <div className='h-70 w-70 border '></div>
- <div className='h-70 w-70 border '></div>
+<div className='flex space-x-2.5 items-center justify-center h-screen text-center text-xl '>
+ <div className='h-80 w-80 border rounded-2xl'>
+  <img src={pic} alt="" />
+ <p>Ayush singh <br />
+  Btech(cse)
+ </p>
+ </div>
+ <div className='h-80 w-80 border rounded-2xl '>
+  <img src={pic1} alt="" />
+  <p>Sankalp singh <br />
+  BCA </p>
+  </div>
+
+ <div className='h-80 w-80 border rounded-2xl '>
+  <img src={pic} alt="" />
+  <p>Sankalp singh <br />
+  BCA </p>
+  </div>
+
+ <div className='h-80 w-80 border rounded-2xl '><img src={pic1} alt="" />    <p>Sankalp singh <br />
+  BCA </p></div>
+
+ <div className='h-80 w-80 border rounded-2xl '><img src={pic} alt="" />    <p>Sankalp singh <br />
+  BCA </p></div>
+</div>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,13 @@ import { createRoot } from 'react-dom/client'
 // import App from './App.jsx'
 // import App from './Demo.jsx'
 // import App from './Props/App.jsx';
-import App from './Tailwind/Tailwind.jsx';
-
+// import App from './Tailwind/Tailwind.jsx';
+import App from './Routing/Routing.jsx';
+import { BrowserRouter } from 'react-router-dom';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </StrictMode>,
 )

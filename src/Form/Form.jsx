1d25e76bcff1e290f4fb56a8d17eb737b3d89043
Komalbaghel08 <baghelkomal512@gmail.com>
2025-09-25 16:07:41 +0530

    form validation

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -1,16 +1,29 @@
 import { useState } from "react";
 
 const App = ()=>{
-    const [val,setval] = useState()
-    const change = (e)=>{
-        console.log(e.target.value);
-        setval(e.target.value)
-    }
+    const [name,setname] = useState("")
+    const [email,setemail] = useState("")
+    const [num,setnum] = useState("")
+    const [pass,setpass] = useState("")
+   
     return (
         <>
             <h1>Form</h1>   
-            <input type="text" onChange={change} />
-            <h1>{val}</h1>
+
+            
+
+            <form action="">
+                <h1>{name}</h1>
+                Name <input type="text" value={name} onChange={(e)=>{setname(e.target.value)}} /> <br /> <br />
+                <h1>{email}</h1>
+                Email <input type="text" value={email} onChange={(e)=>{setemail(e.target.value)}} /><br /> <br />
+                <h1>{num}</h1>
+                Contact <input type="text" value={num} onChange={(e)=>{setnum(e.target.value)}} /><br /> <br />
+                <h1>{pass}</h1>
+                password <input type="text"value={pass} onChange={(e)=>{setpass(e.target.value)}} /><br /> <br />
+              
+               <button type="submit">Sign up</button>
+            </form>
         </>
     )
 

23ede269efc2ba9fdaccaf556b6bbe1454edd9c7
Komalbaghel08 <baghelkomal512@gmail.com>
2025-09-27 15:25:46 +0530

    update

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -1,26 +1,43 @@
 import { useState } from "react";
 
 const App = ()=>{
-    const [name,setname] = useState("")
-    const [email,setemail] = useState("")
-    const [num,setnum] = useState("")
-    const [pass,setpass] = useState("")
-   
+    // const [name,setname] = useState("")
+    // const [email,setemail] = useState("")
+    // const [num,setnum] = useState("")
+    // const [pass,setpass] = useState("")
+    const[formdata , setformData]=useState({
+
+        name:"",
+        email:"",
+        num:"",
+        pass:""
+    })
+   let changeimp =(e)=>{
+      setformData({
+        ...formdata,
+        [e.target.name]:e.target.value
+      })
+   }
+
+
+    let handlesubmit=(e)=>{
+        e.preventDefault() //to prevent form reloading
+        alert("form submitted")
+    }
+
     return (
         <>
             <h1>Form</h1>   
-
             
-
-            <form action="">
-                <h1>{name}</h1>
-                Name <input type="text" value={name} onChange={(e)=>{setname(e.target.value)}} /> <br /> <br />
-                <h1>{email}</h1>
-                Email <input type="text" value={email} onChange={(e)=>{setemail(e.target.value)}} /><br /> <br />
-                <h1>{num}</h1>
-                Contact <input type="text" value={num} onChange={(e)=>{setnum(e.target.value)}} /><br /> <br />
-                <h1>{pass}</h1>
-                password <input type="text"value={pass} onChange={(e)=>{setpass(e.target.value)}} /><br /> <br />
+            <form onSubmit={handlesubmit}>
+                <h1>{formdata.name}</h1>
+                Name <input type="text" name="name" value={formdata.name} onChange = {changeimp}/> <br /> <br />
+                <h1>{formdata.email}</h1>
+                Email <input type="text" name="email" value={formdata.email} onChange={changeimp} /><br /> <br />
+                <h1>{formdata.num}</h1>
+                Contact <input type="text" name="num" value={formdata.num} onChange={changeimp} /><br /> <br />
+                <h1>{formdata.pass}</h1>
+                password <input type="text" name="pass" value={formdata.pass} onChange={changeimp} /><br /> <br />
               
                <button type="submit">Sign up</button>
             </form>

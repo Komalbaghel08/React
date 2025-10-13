b61f837df21aa0e197f13c554ba1f53a1e24686b
Komalbaghel08 <baghelkomal512@gmail.com>
2025-10-13 21:54:22 +0530

    practice

--- src/CRUD.jsx
+++ src/CRUD.jsx
@@ -1,62 +1,111 @@
-import React, { useEffect } from "react";
-import Axios from "axios";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { data } from "react-router-dom";
+
 const CRUD = () => {
-  let [Data, setData] = React.useState([]);
+  const [Data, setData] = useState([]);
+  const [form, setForm] = useState({
+    name: "",
+    age: "",
+    city: "",
+    checkin: "",
+    checkout: "",
+    price: "",
+    people: "",
+    total: "",
+  });
+
+  // Fetch Data (GET)
   useEffect(() => {
-    axios
-      .get("http://localhost:3000/hotel")
-      .then((res) => {
-        console.log(res);
-        console.log(res.data);
-        setData(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    axios.get("http://localhost:3000/hotel").then((res) => setData(res.data))
+      .catch((err) => console.log(err));
   }, []);
 
+  // Handle Delete
+  const del = (id) => {
+    axios.delete("http://localhost:3000/hotel/" + id).then(() => {
+        alert("Data Deleted Successfully");
+        setData(Data.filter((e) => e.id !== id));
+      })
+      .catch((err) => console.log(err));
+  };
 
-const post = ()
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const post = (e) => {
+    e.preventDefault();
+
+    axios.post("http://localhost:3000/hotel", form).then((res) => {
+        alert("Data Added Successfully");
+        setData([...Data, res.data]); 
+        setForm({
+          name: "",
+          age: "",
+          city: "",
+          checkin: "",
+          checkout: "",
+          price: "",
+          people: "",
+          total: "",
+        }); // clear form
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <>
-      <h1>Show data...</h1>
-      <div>
-        {/* {Data.map((e) => (
-          <h2>{e.name}</h2>
-        ))} */}
+      <h1>Hotel Data Management </h1>
 
-        <table border="1">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Age </th>
-              <th>City</th>
-              <th>CheckIN</th>
-              <th>CheckOut</th>                            
-              <th>price</th>
-              <th>people</th>
-              <th>total</th>
+      {/* Input Form */}
+      <form onSubmit={post} style={{ marginBottom: "20px" }}>
+        <input name="name" placeholder="Name" value={form.name} onChange={handleChange} /> <br /> <br /> 
+        <input name="age" placeholder="Age" value={form.age} onChange={handleChange} /> <br /><br /> 
+        <input name="city" placeholder="City" value={form.city} onChange={handleChange} /> <br /><br /> 
+        <input name="checkin" placeholder="CheckIN" value={form.checkin} onChange={handleChange} /> <br /><br /> 
+        <input name="checkout" placeholder="CheckOut" value={form.checkout} onChange={handleChange} /> <br /><br /> 
+        <input name="price" placeholder="Price" value={form.price} onChange={handleChange} /> <br /><br /> 
+        <input name="people" placeholder="People" value={form.people} onChange={handleChange} /> <br /><br /> 
+        <input name="total" placeholder="Total" value={form.total} onChange={handleChange} /> <br /><br /> 
+        <button type="submit">Add Data</button>
+      </form>
+
+      {/* Table */}
+      <table border="1" cellPadding="8">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>City</th>
+            <th>CheckIN</th>
+            <th>CheckOut</th>
+            <th>Price</th>
+            <th>People</th>
+            <th>Total</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Data.map((e) => (
+            <tr key={e.id}>
+              <td>{e.name}</td>
+              <td>{e.age}</td>
+              <td>{e.city}</td>
+              <td>{e.checkin}</td>
+              <td>{e.checkout}</td>
+              <td>{e.price}</td>
+              <td>{e.people}</td>
+              <td>{e.total}</td>
+              <td>
+                <button onClick={() => del(e.id)}>Delete</button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {Data.map((e) => (
-              <tr key={e.id}>
-                    <td>{e.name}</td>
-                    <td>{e.age}</td>
-                    <td>{e.city}</td>
-                    <td>{e.checkin}</td>
-                    <td>{e.checkout}</td>
-                    <td>{e.price}</td>
-                    <td>{e.people}</td>
-                    <td>{e.total}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
+
 export default CRUD;
